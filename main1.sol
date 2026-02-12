be49bcfd3ac08be71318f47eba71a56de52ee72d
vontavel <gitlebam@gmail.com>
2026-02-12 15:57:25 +0000

    Update main1.sol

--- main1.sol
+++ main1.sol
@@ -33,3 +33,38 @@ contract PsychicOctoGiggle is ReentrancyGuard, Pausable {
     event EpochRolled(uint256 previousEpoch, uint256 newEpoch, uint256 atBlock);
     event CuratorTopped(uint256 amount, address indexed from, uint256 newTreasuryBalance);
 
+    error ZingerErr_SlotEmpty();
+    error ZingerErr_NotCurator();
+    error ZingerErr_SlotAlreadyFilled();
+    error ZingerErr_ZeroSlotId();
+    error ZingerErr_ClaimCooldownActive();
+    error ZingerErr_EpochWindowNotReached();
+    error ZingerErr_ClaimCapExceeded();
+    error ZingerErr_InvalidCategory();
+    error ZingerErr_NoPunchlinesRemaining();
+    error ZingerErr_ZeroAddress();
+    error ZingerErr_JokeSlotCapReached();
+    error ZingerErr_NotEpochRoller();
+    error ZingerErr_InvalidSlotIndex();
+
+    uint256 public constant MAX_JOKE_SLOTS = 128;
+    uint256 public constant JOKE_EPOCH_BLOCKS = 256;
+    uint256 public constant PUNCHLINE_CLAIM_PER_JOKE = 100;
+    uint256 public constant MAX_CLAIM_PER_EPOCH = 1000;
+    uint256 public constant CLAIM_COOLDOWN_BLOCKS = 32;
+    uint256 public constant CATEGORY_COUNT = 6;
+    bytes32 public constant ZINGER_DOMAIN =
+        bytes32(uint256(0x1f2e3d4c5b6a7980e1d2c3b4a5968778695a4b3c2d1e0f9a8b7c6d5e4f3a2b1));
+
+    address public immutable zingerCurator;
+    address public immutable zingerTreasury;
+    uint256 public immutable genesisBlock;
+    bytes32 public immutable jokeSeed;
+
+    uint256 public currentEpoch;
+    uint256 public totalJokesServed;
+    uint256 public totalPunchlinesClaimed;
+    uint256 public treasuryBalance;
+    uint256 public activeSlotCount;
+
+    mapping(bytes32 => JokeSlot) private _slots;
